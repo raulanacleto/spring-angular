6132f9dd99bab80cc87c90044ddb55805a394c61
raulanacleto <raulanacletosilva@gmail.com>
2019-09-02 21:14:23 -0300

    atualizado postgres de 9.6 para 10

--- src/main/resources/db/migration/V01__teste.sql
+++ src/main/resources/db/migration/V01__teste.sql
@@ -1,5 +1,25 @@
-insert into categoria (nome) values ('Lazer');
-insert into categoria (nome) values ('Alimentação');
-insert into categoria (nome) values ('Supermercado');
-insert into categoria (nome) values ('Farmácia');
-insert into categoria (nome) values ('Outros');
+CREATE SEQUENCE public.categoria_codigo_seq
+    INCREMENT 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    START 1
+    CACHE 1;
+ALTER TABLE public.categoria_codigo_seq
+    OWNER TO postgres;
+
+CREATE TABLE categoria
+(
+    codigo integer NOT NULL primary key DEFAULT nextval('categoria_codigo_seq'::regclass),
+    nome   VARCHAR(50) NOT NULL
+);
+
+insert into categoria (nome)
+values ('Lazer');
+insert into categoria (nome)
+values ('Alimentação');
+insert into categoria (nome)
+values ('Supermercado');
+insert into categoria (nome)
+values ('Farmácia');
+insert into categoria (nome)
+values ('Outros');
